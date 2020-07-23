23f3ea540120d31648fca6245bd545dd20fc1899
Mark Liu <ldtheended@163.com>
2020-07-23 13:07:08 +0800

    a0.05（Webpack02案例example07——output设置出口）

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,19 @@
+const path = require('path');
+
 module.exports = {
     //配置项
     // entry: './src/index.js',    //设置入口
     mode: 'development',
     entry: {
         index: './src/index.js',
-        list: './src/list.js'
+        list: './src/list.js',
     },
+
+    output: {
+        // __dirname当前该文件所在目录（绝对路径）
+        path: path.resolve(__dirname, "dist"),  // 需要path，需要引入
+        // filename: "bundle.js",  //单文件出口的设置一个filename即可
+        filename: "[name].js"   // [name] 内置变量占位符（多出口），占位符默认就是key
+    },
+
 }
