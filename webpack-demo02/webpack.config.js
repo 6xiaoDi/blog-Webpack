9b948acbe3835600493b5afa3eefc51ce4b85a38
Mark Liu <ldtheended@163.com>
2020-07-24 12:07:39 +0800

    a0.20（Webpack03——example01-1使用HtmlWebpackPlugin插件生成html自动引入js）

--- webpack-demo02/webpack.config.js
+++ webpack-demo02/webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 
     mode: 'development',
+
     entry: {
-        'test': './src/test.js',
+        // 'test': './src/test.js',
+
+        'template': './src/template.js'
     },
     output: {
         // 这个路径是默认会被加入 devServer 的 contentBase 中
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].js"
     },
+
+
     module: {
         rules: [
             {
@@ -19,4 +25,13 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 模板文件存放的目录
+            template: "./html/template.html",
+            // 生成（打包）后的html存放目录
+            filename: "app.html"
+        }),
+    ],
 };
