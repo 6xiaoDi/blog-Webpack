5e7e945ee962031981ae46905cc9880d0fae0909
Mark Liu <ldtheended@163.com>
2020-07-25 23:12:18 +0800

    a0.34（Webpack03——example07-1-配置nodejs的koa）

---
+++ server01/app.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+
+const app = new Koa();
+const router = new KoaRouter();
+
+router.get('/', async ctx => {
+    ctx.body = '大前端开发';
+})
+
+app.use(router.routes())
+
+app.listen(7777);
