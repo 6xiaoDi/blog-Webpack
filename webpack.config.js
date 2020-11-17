ee1db36c7b58dedccb0857324cec11ed15b699fb
6xd <ldtheended@163.com>
2020-11-17 19:32:01 +0800

    a0.42（webpack构建——html-webpack-plugin ）

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'index.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+        })
+    ],
 }
